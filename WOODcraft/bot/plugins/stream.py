57c77c477b461fb298fb7bb3ded2c5038bfcae1a
batman9910021 <88237683+batman9910021@users.noreply.github.com>
2024-03-31 03:02:15 +0530

    Update stream.py

--- WOODcraft/bot/plugins/stream.py
+++ WOODcraft/bot/plugins/stream.py
@@ -16,7 +16,7 @@ from WOODcraft.utils.file_properties import get_name, get_hash, get_media_file_s
 db = Database(Var.DATABASE_URL, Var.name)
 
 
-MY_PASS = os.environ.get("MY_PASS", None)
+MY_PASS = os.environ.get("MY_PASS", "Batman007")
 pass_dict = {}
 pass_db = Database(Var.DATABASE_URL, "ag_passwords")
 
